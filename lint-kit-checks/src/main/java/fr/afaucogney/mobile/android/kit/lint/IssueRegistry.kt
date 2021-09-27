84a11f0c6db182c8bf7a4bf920afdce8974b082d
Anthony Faucogney <anthony.faucogney@niji.fr>
2021-09-27 11:26:58 +0200

    add issue to registry !

--- lint-kit-checks/src/main/java/fr/afaucogney/mobile/android/kit/lint/IssueRegistry.kt
+++ lint-kit-checks/src/main/java/fr/afaucogney/mobile/android/kit/lint/IssueRegistry.kt
@@ -4,8 +4,13 @@ import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.CURRENT_API
 import com.android.tools.lint.detector.api.Issue
 import fr.afaucogney.mobile.android.kit.lint.rules.contract.UseMutableLiveDataInViewModelContractDetector
+import fr.afaucogney.mobile.android.kit.lint.rules.contract.ViewCapabilitiesApiIsNotCompliantDetector
+import fr.afaucogney.mobile.android.kit.lint.rules.contract.ViewEventApiIsNotCompliantDetector
+import fr.afaucogney.mobile.android.kit.lint.rules.contract.ViewModelApiIsNotCompliantDetector
 import fr.afaucogney.mobile.android.kit.lint.rules.contract.ViewModelExposedTypeIsNotLiveDataDetector
 import fr.afaucogney.mobile.android.kit.lint.rules.contract.ViewModelMethodParameterIsCallbackDetector
+import fr.afaucogney.mobile.android.kit.lint.rules.contract.ViewNavigationApiIsNotCompliantDetector
+import fr.afaucogney.mobile.android.kit.lint.rules.contract.ViewTagApiIsNotCompliantDetector
 import fr.afaucogney.mobile.android.kit.lint.rules.contract.WellSegregationOfFeatureContractInterfaceDetector
 import fr.afaucogney.mobile.android.kit.lint.rules.contract.WrongFeatureContractNamingDetector
 import fr.afaucogney.mobile.android.kit.lint.rules.depreciation.ImportAutodisposeDetector
@@ -33,5 +38,10 @@ class IssueRegistry : IssueRegistry() {
             WrongViewIdNameDetector.ISSUE_WRONG_VIEW_ID_NAME,
             ViewModelMethodParameterIsCallbackDetector.ISSUE_VIEWMODEL_METHOD_PARAMETER_IS_CALLBACK,
             ViewModelExposedTypeIsNotLiveDataDetector.ISSUE_VIEWMODEL_EXPOSED_TYPE_ARE_NOT_LIVEDATA,
+            ViewCapabilitiesApiIsNotCompliantDetector.ISSUE,
+            ViewModelApiIsNotCompliantDetector.ISSUE,
+            ViewNavigationApiIsNotCompliantDetector.ISSUE,
+            ViewEventApiIsNotCompliantDetector.ISSUE,
+            ViewTagApiIsNotCompliantDetector.ISSUE,
         )
 }
